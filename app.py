30077db4a4226ab1af6d6ccf1903e00becdb7e75
PriyaGhosal <priyaghosal2005@gmail.com>
2026-02-21 19:33:52 +0530

    Configured app for Render deployment

--- app.py
+++ app.py
@@ -58,7 +58,10 @@ def download_csv():
 def download_image():
     return send_file("static/uploads", as_attachment=True)
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 
